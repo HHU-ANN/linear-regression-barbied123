aed498d5891ad106dab60f95a85b98e6de4d7dc5
barbied123 <101971342+barbied123@users.noreply.github.com>
2023-05-06 21:57:22 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,12 +8,12 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def ridge(data,a=0.2):
+def ridge(data,a=0.1):
     x,y=read_data()
     weight=np.matmul(np.linalg.inv(np.matmul(x.T,x)+np.eye(x.shape[1]*a)),np.matmul(x.T,y))
     return weight@data
     
-def lasso(data,t=0.05,a=0.1):
+def lasso(data,t=1,a=0.1):
     x,y=read_data()
     for i in range(400):
         M=w-wt*np.matmul(x.T,x)+t*np.matmul(x.T,y)
